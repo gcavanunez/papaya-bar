e6f3620b5c73ae203216e8d386d1a13e5160d2f4
Guillermo Cava <gcavanunez@gmail.com>
2024-06-17 21:30:07 -0500

    updates vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
 import { crx } from '@crxjs/vite-plugin'
 
 //@ts-ignore
 import manifest from './manifest.json' // Node 14 & 16
 
 import { execSync } from 'child_process'
-// const childProcess = require('child_process')
 let lastCommitHash = ''
 try {
 	lastCommitHash = execSync('git rev-parse --short HEAD').toString().trim()
@@ -16,21 +15,15 @@ try {
 }
 console.log({ lastCommitHash })
 
-// https://vitejs.dev/config/
 export default defineConfig({
 	define: {
 		__COMMIT_HASH__: JSON.stringify(lastCommitHash),
 	},
-	plugins: [
-		vue({
-			// reactivityTransform: true,
-		}),
-		crx({ manifest }),
-	],
+	plugins: [vue(), crx({ manifest })],
 
 	resolve: {
 		alias: {
-			'@': resolve(__dirname, 'src'),
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
 		},
 	},
 })
